f45895abd3b290b9f1798371275ae841cbbd3340
Soumen Sarkar <soumen.rim@gmail.com>
2019-07-13 22:46:01 +0530

    First commit

---
+++ js/custom-script.js
@@ -0,0 +1,10 @@
+$(window).load(function(){	
+	
+    //Script for dropdown
+    $('ul.nav li.dropdown').hover(function() {
+        $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeIn(500);
+    }, function() {
+        $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeOut(500);
+    });
+	
+});
